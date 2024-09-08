a8aca3d4e513c4a3ec458d43ae8daccc77cc8d54
Phil Träger <darkoberd00@gmail.com>
2024-09-08 22:24:52 +0200

    move setParser to debug

--- src/commands/debug/index.ts
+++ src/commands/debug/index.ts
@@ -2,5 +2,6 @@ import { category } from "../../utils";
 import ping from "./ping";
 import displayUser from "./displayUser";
 import clear from "./clear";
+import setParser from "./setParser";
 
-export default category("Debug", [ping, displayUser, clear]);
+export default category("Debug", [ping, displayUser, clear, setParser]);
